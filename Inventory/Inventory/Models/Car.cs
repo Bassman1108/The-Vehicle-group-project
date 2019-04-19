4d4052c2133a4351ba65ce8823c00a41f824c0ed
Randy King <Randy.D.King-1@ou.edu>
2019-04-19 12:18:28 -0500

    project start

---
+++ Inventory/Inventory/Models/Car.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Inventory.Models
+{
+    public class Car
+    {
+        public string VIN { get; set; }
+        public int Year { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public double Cost { get; set; }
+    }
+}
