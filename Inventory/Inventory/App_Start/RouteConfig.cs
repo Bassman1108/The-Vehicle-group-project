2f8e98acf1a8655af2d284610a68a73e4328e2f2
Student <Student@PCB-STULAP-09>
2019-04-26 11:45:48 -0500

    home route

--- Inventory/Inventory/App_Start/RouteConfig.cs
+++ Inventory/Inventory/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Inventory
             routes.MapRoute(
             name: "Vehicles",
             url: "{controller}/{action}/{id}",
-            defaults: new { controller = "Vehicles", action = "Index", id = UrlParameter.Optional }
+            defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
             
     
